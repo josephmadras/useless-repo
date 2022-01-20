72e78afa8f85df0c1048acc95c50ce915800f23d
Joseph Madras <josephkabemba77@gmail.com>
2022-01-20 11:44:08 +0100

    feat: create random number generator

--- index.js
+++ index.js
@@ -5,3 +5,9 @@ function computeSalary() {
 const tax = 500;
 
 console.log("Joseph Madras");
+
+function displayRandomNumber() {
+  console.log(Math.round(Math.random() * 100));
+}
+
+displayRandomNumber();
